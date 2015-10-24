05f14688f74a3985971234fd328799c415eb5504
Eddie Lin <elin@mozilla.com>
2015-10-24 17:54:42 +0800

    Initial check-in robot arm implementations.

--- www/js/config.js
+++ www/js/config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
   DEVICE_NAME: 'BleFirmata',
-  DEVICE_ADDRESS: 'd0:6a:cf:58:ee:bd',
+  DEVICE_ADDRESS: 'db:7a:0e:53:71:5a',
   DEVICE_ADDRESS_IOS: '625b069e-f4bf-028d-e256-e8afd57b2fd8',
   SOCKET_IO_SERVER: 'ws://localhost:3000'
 };
--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,9 @@
-/* global five,BleSerialPort,VirtualJoystick */
+/* global five,BleSerialPort */
 'use strict';
 
-function initCarControl() {
-  console.log('initialize car controls');
-  var joystickElem = document.getElementById('joystick');
-  var joystick = new VirtualJoystick({
-    container: joystickElem,
-    limitStickTravel: true,
-    stickRadius: 60,
-    mouseSupport: !window.cordova
-  });
+function initControls() {
+  console.log('initialize arm controls');
+  var controlsElem = document.getElementById('controls');
 
   var bsp;
   if (window.cordova) {
@@ -33,101 +27,46 @@ function initCarControl() {
     });
   }
 
+  function acc2deg(acc) {
+    if (acc < 2.4) {
+      acc = 2.4;
+    } else if (acc > 7.2) {
+      acc = 7.2;
+    }
+    return (acc - 2.4) / 4.8 * 90 + 45;
+  }
+
   bsp.connect().then(function() {
     console.log('BSP connected');
     var board = new five.Board({port: bsp, repl: false});
     board.on('ready', function() {
       console.log('Arduino connected!');
-      joystickElem.style.background = '#000000';
+      controlsElem.style.background = '#000000';
       var led = new five.Led(7);
       led.on();
-      var motorLeft = new five.Motor({
-        invertPWM: true,
-        pins: {
-          pwm: 5,
-          dir: 4
-        }
+      var xServo = new five.Servo({
+        pin: 6,
+        range: [45, 135],
+        startAt: 90
       });
-      var motorRight = new five.Motor({
-        invertPWM: true,
-        pins: {
-          pwm: 3,
-          dir: 2
-        }
+      var yServo = new five.Servo({
+        pin: 5,
+        range: [45, 135],
+        startAt: 90
       });
-
-      var COMMAND_INTERVAL = 100;
-      var MOVE_THRESHOLD = 20;
-      var MIN_SPEED = 50;
-      var MAX_SPEED = 255;
-      var FACTOR = 3.5;
-
-      function moveCar() {
-        var dx = joystick.deltaX() * FACTOR;
-        var dy = joystick.deltaY() * FACTOR;
-
-        if (Math.abs(dx) < MOVE_THRESHOLD && Math.abs(dy) < MOVE_THRESHOLD) {
-          motorLeft.stop();
-          motorRight.stop();
-          return;
-        }
-
-        var ls = Math.abs(dy) + MIN_SPEED - MOVE_THRESHOLD;
-        var rs = Math.abs(dy) + MIN_SPEED - MOVE_THRESHOLD;
-        if (dx > MOVE_THRESHOLD) {
-          ls += dx + MIN_SPEED - MOVE_THRESHOLD;
-        } else if (dx < -MOVE_THRESHOLD) {
-          rs -= dx - MIN_SPEED + MOVE_THRESHOLD;
-        }
-        ls = ls > MAX_SPEED ? MAX_SPEED : ls;
-        rs = rs > MAX_SPEED ? MAX_SPEED : rs;
-        if (dy < -MOVE_THRESHOLD) {
-          console.log('forward', ls, rs);
-          motorLeft.forward(ls);
-          motorRight.forward(rs);
-        } else if (dy > MOVE_THRESHOLD) {
-          console.log('reverse', ls, rs);
-          motorLeft.reverse(ls);
-          motorRight.reverse(rs);
-        }
-      }
-
-      var prevMoveTime = new Date().getTime();
-
-      function onJoystickTouch(e) {
-        switch (e.type) {
-          case 'touchstart':
-          case 'mousedown':
-            joystick.onTouch = true;
-            console.log('start');
-            break;
-          case 'touchend':
-          case 'mouseup':
-            joystick.onTouch = false;
-            console.log('end');
-            setTimeout(function() {
-              motorLeft.stop();
-              motorRight.stop();
-            }, COMMAND_INTERVAL * 2);
-            break;
-          case 'touchmove':
-          case 'mousemove':
-            if (joystick.onTouch && new Date().getTime() -
-              prevMoveTime > COMMAND_INTERVAL) {
-              prevMoveTime = new Date().getTime();
-              console.log('move', joystick.deltaX(), joystick.deltaY());
-              moveCar();
-            }
-            break;
-        }
-      }
-
-      joystickElem.addEventListener('touchstart', onJoystickTouch);
-      joystickElem.addEventListener('touchend', onJoystickTouch);
-      joystickElem.addEventListener('touchmove', onJoystickTouch);
-      joystickElem.addEventListener('mousedown', onJoystickTouch);
-      joystickElem.addEventListener('mouseup', onJoystickTouch);
-      joystickElem.addEventListener('mousemove', onJoystickTouch);
+      navigator.accelerometer.watchAcceleration(
+          function(acceleration) {
+            //console.log(acceleration);
+            var x = acc2deg(acceleration.x + 5);
+            var y = acc2deg(acceleration.y);
+            console.log(x, y);
+            xServo.to(x);
+            yServo.to(y);
+          },
+          function() {
+            console.error('Accelerometer Error!');
+          },
+          { frequency: 1000 });
     });
   });
 }
@@ -144,7 +83,11 @@ var app = {
 
   onDeviceReady: function() {
     app.receivedEvent('deviceready');
-    initCarControl();
+    try {
+      initControls();
+    } catch (e) {
+      console.error(e);
+    }
   },
 
   receivedEvent: function(id) {
